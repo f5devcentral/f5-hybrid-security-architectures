3a71a64814619d8cbfeca722375f715161c9f06e
Cameron Delano <knowbase@icloud.com>
2023-01-04 09:10:20 -0800

    refactor build 4

--- juice-shop/variables.tf
+++ juice-shop/variables.tf
@@ -2,33 +2,39 @@
 variable "project_prefix" {
   type        = string
   description = "This value is inserted at the beginning of each AWS object (alpha-numeric, no special character)"
+  default = data.tf_outputs.infra.values.project_prefix
 }
 variable "resource_owner" {
   type        = string
   description = "owner of the deployment, for tagging purposes"
+  default = data.tf_outputs.infra.values.resource_owner
 }
 variable "build_suffix" {
   type = string
   description = "random id"
+  default = data.tf_outputs.infra.values.build_suffix
 }
 
 #AWS Global
 variable "aws_region" {
   description = "aws region"
   type        = string
+  default = data.tf_outputs.infra.values.aws_region
 }
 variable "azs" {
   description = "Availability Zones"
   type        = list
+  default = data.tf_outputs.infra.values.azs
 }
 variable "vpc_id" {
   type        = string
   description = "The AWS network VPC ID"
-  default     = null
+  default     = data.tf_outputs.infra.values.vpc_id
 }
 variable "vpc_main_route_table_id" {
   type = string
   description = "Main route table id"
+  default =  data.tf_outputs.infra.values.vpc_main_route_table_id
 }
 variable "private_cidr_blocks" {
   type        = list(any)
@@ -37,6 +43,7 @@ variable "private_cidr_blocks" {
 }
 variable "app_cidr" {
   type = string
+  default = data.tf_outputs.infra.values.app_cidr
 }
 #EC2
 /*
@@ -50,7 +57,7 @@ variable "public_subnet_ids" {
 variable "internal_sg_id" {
   type        = string
   description = "Internal securitiy group ID"
-  default     = null
+  default     = data.tf_outputs.infra.values.internal_sg_id
 }
 variable "ssh_key" {
   type        = string
