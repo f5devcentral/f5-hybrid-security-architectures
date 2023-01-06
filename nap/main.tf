4c3686c6282db7f465674d43f24d6832d9e2bd58
Cameron Delano <knowbase@icloud.com>
2023-01-05 17:06:51 -0800

    test deploy

--- nap/main.tf
+++ nap/main.tf
@@ -4,9 +4,12 @@ provider "aws" {
 provider "kubernetes" {
     host = local.host
     cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+    token = data.aws_eks_cluster_auth.auth.token
+    /*
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
     command     = "aws"
   }
+  */
 }
