56520cf48fa3d0c618c5ef6b2ef317f24f167408
Cameron Delano <knowbase@icloud.com>
2023-01-06 13:08:30 -0800

    >.<

--- nap/nap.tf
+++ nap/nap.tf
@@ -2,7 +2,9 @@ resource "helm_release" "nginx-plus-ingress" {
     chart = "nginx-plus-ingress"
     name = "nginx-plus-ingress"
     namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
-    values = [file("charts/nignx-plus-ingress/values.yaml")]
+    values = [file("./charts/nginx-plus-ingress/values.yaml")]
 
-
+    depends_on = [
+      kubernetes_secret.docker-registry
+    ]
 }
