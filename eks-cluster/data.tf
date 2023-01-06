798c3a2a9f47fdfb01f14b1f9a800f607cc7c5bd
Cameron Delano <knowbase@icloud.com>
2023-01-06 11:16:47 -0800

    demo

--- eks-cluster/data.tf
+++ eks-cluster/data.tf
@@ -2,8 +2,4 @@ data "tfe_outputs" "infra" {
   organization = "knowbase"
   workspace = "xc-bigip-test"
 }
-/*
-data "aws_eks_cluster_auth" "auth" {
-  name = aws_eks_cluster.eks-tf.name
-}
-*/
+
--- nap/locals.tf
+++ nap/locals.tf
@@ -3,5 +3,4 @@ locals {
   host = data.tfe_outputs.eks.values.cluster_endpoint
   cluster_ca_certificate = data.tfe_outputs.eks.values.kubeconfig-certificate-authority-data
   cluster_name = data.tfe_outputs.eks.values.cluster_name
-  #token = data.tfe_outputs.eks.values.token
 } 
