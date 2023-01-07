b4ea84073ef4331b87b1f51d540ad8a9104292d6
Cameron Delano <knowbase@icloud.com>
2023-01-06 16:15:25 -0800

    arcadia ingress

---
+++ arcadia/ingress.tf
@@ -0,0 +1,43 @@
+resource "kubernetes_ingress" "arcadia" {
+  wait_for_load_balancer = true
+  metadata {
+    name = "arcadia"
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
+  spec {
+    rule {
+      http {
+        path {
+          path = "/"
+          backend {
+            service_name = kubernetes_service.main.metadata.0.name
+            service_port = 80
+          }
+        }
+        path {
+          path = "/files"
+          backend {
+            service_name = kubernetes_service.backend.metadata.0.name
+            service_port = 80
+          }
+        }
+        path {
+          path = "/api"
+          backend {
+            service_name = kubernetes_service.app2.metadata.0.name
+            service_port = 80
+          }
+        }
+        path {
+          path = "/app3"
+          backend {
+            service_name = kubernetes_service.app3.metadata.0.name
+            service_port = 80
+          }
+        }
+      }
+    }
+  }
+}
