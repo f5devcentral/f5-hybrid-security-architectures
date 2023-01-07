750cdce2ad1c607c6f15ee2c986420dc70911f9a
Cameron Delano <knowbase@icloud.com>
2023-01-07 10:43:47 -0800

    7/1/2023-10:28:00-PST TFC nonsensitive

--- xc/outputs.tf
+++ xc/outputs.tf
@@ -1,6 +1,6 @@
 
 output "xc_lb_name" {
-  value = volterra_http_loadbalancer.lb_https.name
+  value = nonsensitive(volterra_http_loadbalancer.lb_https.name)
 }
 output "xc_waf_name" {
   value = nonsensitive(volterra_app_firewall.waap-tf.name)
