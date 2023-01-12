306ca776bfe9e656fe63f4cf3eb112927ba71ea2
a500drvr <a500drvr@gmail.com>
2023-01-11 16:34:21 -0800

    vaiables change

--- eks-cluster/variables.tf
+++ eks-cluster/variables.tf
@@ -1,18 +1,4 @@
 #Project Globals
-variable "project_prefix" {
-  type        = string
-  default     = "demo"
-  description = "This value is inserted at the beginning of each object (alpha-numeric, no special character)"
-}
-variable "resource_owner" {
-  type        = string
-  description = "owner of the deployment, for tagging purposes"
-  default     = "myName"
-}
-variable "build_suffix" {
-  description = "random id"
-  type = string
-}
 variable "admin_src_addr" {
   type        = string
   description = "Allowed Admin source IP prefix"
@@ -24,25 +10,6 @@ variable "tf_cloud_organization" {
   description = "TF cloud org (Value set in TF cloud)"
 }
 #AWS
-variable "vpc_id" {
-  type        = string
-  description = "The AWS network VPC ID"
-  default     = null
-}
-variable "vpc_main_route_table_id" {
-  type = string
-  description = "Main route table id"
-}
-variable "eks_cidr" {
-  type = string
-}
-variable "public_subnet_ids" {
-  type = list(any)
-}
-variable "azs" {
-  description = "Availability Zones"
-  type        = list
-}
 variable "eks_addons" {
   type = list(object({
     name    = string
