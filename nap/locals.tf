58631a68b63bdb9a75abdac4f8961db19b97587e
Cameron Delano <knowbase@icloud.com>
2023-01-17 07:29:23 -0800

    Workflow fix + Monitoring

--- nap/locals.tf
+++ nap/locals.tf
@@ -1,5 +1,6 @@
 locals {
   project_prefix = data.tfe_outputs.infra.values.project_prefix
+  build_suffix = data.tfe_outputs.infra.values.build_suffix
   aws_region = data.tfe_outputs.infra.values.aws_region
   host = data.tfe_outputs.eks.values.cluster_endpoint
   cluster_ca_certificate = data.tfe_outputs.eks.values.kubeconfig-certificate-authority-data
--- nap/nap.tf
+++ nap/nap.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "nginx-plus-ingress" {
-    name = local.project_prefix
+    name = format("%s-nap-%s", local.project_prefix, local.build_suffix)
     repository = "https://helm.nginx.com/stable"
     chart = "nginx-ingress"
     namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
