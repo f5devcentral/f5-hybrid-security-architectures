3506b4760ed0445bd078eb5fe80c4a5d09deda74
Cameron Delano <62901589+knowbase@users.noreply.github.com>
2023-04-14 20:15:09 -0700

    Update outputs.tf

--- nic/outputs.tf
+++ nic/outputs.tf
@@ -1,5 +1,5 @@
 output "external_name" {
-    value = data.kubernetes_service.nginx-service.status.0.load_balancer.0.ingress.0.hostname
+    value = data.kubernetes_service_v1.nginx-service.status.0.load_balancer.0.ingress.0.hostname
 }
 output "external_port" {
     value = data.kubernetes_service_v1.nginx-service.spec.0.port.0.port
