635d5062af7b0c98ca5264c16cea7e9d295f2012
Mohammed Janibasha <jani.basha.5000@gmail.com>
2024-08-05 15:48:38 +0530

    Update nap.tf

--- nap/nap.tf
+++ nap/nap.tf
@@ -3,7 +3,7 @@ resource "helm_release" "nginx-plus-ingress" {
     name = format("%s-nap-%s", local.project_prefix, local.build_suffix)
     repository = "https://helm.nginx.com/stable"
     chart = "nginx-ingress"
-    version = "0.16.2"
+    #version = "0.16.2"
     namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
     values = [file("./charts/nginx-app-protect/values.yaml")]
 
@@ -16,7 +16,7 @@ resource "helm_release" "nginx-plus-ingresslink" {
     name = format("%s-nap-%s", local.project_prefix, local.build_suffix)
     repository = "https://helm.nginx.com/stable"
     chart = "nginx-ingress"
-    version = "0.16.2"
+    #version = "0.16.2"
     namespace = kubernetes_namespace.nginx-ingress.metadata[0].name
     values = [templatefile("./charts/nap-ingresslink/values.yaml", { app = "${local.app}"})]
 
