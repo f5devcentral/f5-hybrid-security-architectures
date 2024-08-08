85697f905294bc04691ca2c522ab844a1f9d09d4
Mohammed Janibasha <jani.basha.5000@gmail.com>
2024-08-08 20:11:16 +0530

    Update data.tf

--- nap/data.tf
+++ nap/data.tf
@@ -21,7 +21,7 @@ data "aws_eks_cluster_auth" "auth" {
 }
 data "kubernetes_service_v1" "nginx-service" {
   metadata {
-    name = try(format("%s-%s", helm_release.nginx-plus-ingress.0.name, helm_release.nginx-plus-ingress.0.chart), format("%s-%s", helm_release.nginx-plus-ingresslink.0.name, helm_release.nginx-plus-ingresslink.0.chart))
+    name = try(format("%s-%s-controller", helm_release.nginx-plus-ingress.0.name, helm_release.nginx-plus-ingress.0.chart), format("%s-%s", helm_release.nginx-plus-ingresslink.0.name, helm_release.nginx-plus-ingresslink.0.chart))
     namespace = try(helm_release.nginx-plus-ingress[0].namespace, helm_release.nginx-plus-ingresslink[0].namespace)
   }
 }
