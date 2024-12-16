5221a3b185188b863e8325d0f905e5fc7a03687d
Ishika RAY <i.ray@f5.com>
2024-12-16 16:07:24 +0530

    updated

--- xc/locals.tf
+++ xc/locals.tf
@@ -3,7 +3,7 @@ locals {
   build_suffix = data.tfe_outputs.infra.values.build_suffix
   origin_bigip = try(data.tfe_outputs.bigip[0].values.bigip_public_vip, "")
   origin_nginx = try(data.tfe_outputs.nap[0].values.external_name, data.tfe_outputs.nic[0].values.external_name, "")
-  origin_server = "${coalesce(local.origin_bigip, local.origin_nginx, var.serviceName)}"
+  origin_server = "${coalesce(local.origin_nginx, local.origin_bigip, var.serviceName)}"
   origin_port = try(data.tfe_outputs.nap[0].values.external_port, data.tfe_outputs.nic[0].values.external_port, "80")
   dns_origin_pool = local.origin_nginx != "" ? true : false 
 }
